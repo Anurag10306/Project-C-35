80fe0580d1123dd771c54e730254e14f16e024f7
Anurag10306 <61738030+Anurag10306@users.noreply.github.com>
2020-05-12 14:38:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database;
+var voter, form, start1;
+var canvas;
+var voterCount = 0;
+var gameState = 0;
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+
+
+  start1 = new Start();
+  start1.getState();
+
+
+  form = new Form();
+  form.display();
+  voter = new Voter();
+  voter.getCount();
+
+}
+
+function draw() {
+  background("grey");  
+
+}
